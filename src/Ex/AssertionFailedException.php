502b390ef177e8946319167608e6c718c9041cba
Matthes <matthes@leuffen.de>
2020-03-25 11:15:24 +0100

    update

--- src/Ex/AssertionFailedException.php
+++ src/Ex/AssertionFailedException.php
@@ -16,7 +16,17 @@ class AssertionFailedException extends \Exception
     {
         $this->expected = $expected;
         $this->actual = $actual;
-        parent::__construct($message, $code, $previous);
+
+        if (is_array($expected) || is_array($actual) || ( (is_string($actual) || is_string($expected)) && (strlen($expected) > 50 || strlen($actual) > 50) )) {
+            file_put_contents("/tmp/expected.out", print_r ($expected));
+            file_put_contents("/tmp/actual.out", print_r($actual));
+            $msg = "Expected (/tmp/expeced.out) not equals acutal (/tmp/actual.out).";
+
+        } else {
+            $msg = "Expected '$expected' != '$actual'";
+        }
+
+        parent::__construct($message . " $msg", $code, $previous);
     }
 
 
